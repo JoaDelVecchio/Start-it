4be7f1fc2ce5970cae642b4e911aa6ce03cf81af
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-12-12 19:43:50 -0300

    Delete unused things

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "../components/Navbar";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
 import { Inter } from "next/font/google";
@@ -8,16 +7,6 @@ import { Inter } from "next/font/google";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
