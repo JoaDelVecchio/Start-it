54b7291bf16357feffb1d9198304277830a19735
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-12-12 19:50:36 -0300

    Delete unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "easymde/dist/easymde.min.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
